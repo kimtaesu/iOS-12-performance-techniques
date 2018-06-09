a1904b6ceb640464f9132b8d5d0981fb19327912
Swain Molster <swain.molster@gmail.com>
2018-06-09 16:42:59 -0400

    Adding unit test for cellForRow

--- blog-ios12-performanceTests/blog_ios12_performanceTests.swift
+++ blog-ios12-performanceTests/blog_ios12_performanceTests.swift
@@ -11,5 +11,30 @@ import XCTest
 
 class blog_ios12_performanceTests: XCTestCase {
     
+    func testCellForRowPerformance() {
+        let articlesVC = ArticleListViewController()
+        
+        articlesVC.articles = [dummyArticle]
+        
+        let dummyTableView = UITableView()
+        dummyTableView.register(UINib(nibName: "ArticleCell", bundle: nil), forCellReuseIdentifier: ArticleCell.reuseID)
+        
+        self.measure {
+            let _ = articlesVC.tableView(dummyTableView, cellForRowAt: IndexPath(row: 0, section: 0))
+        }
+    }
     
+    let dummyArticle: Article =
+        Article(
+            source: Article.Source(id: nil, name: "nothing"),
+            author: "CapTech Consulting",
+            title: "This App Works Great",
+            description: nil,
+            url: nil,
+            urlToImage: nil,
+            publishedAt: "2018-06-09T18:48:00Z",
+            publishedAtDate: nil,
+            displayDate: nil,
+            nameHighlightedTitle: nil
+        )
 }
